0f86f0bf0453129eb79c665c96a06c594b30ec75
Ankit Raj <147795844+lucifer5094@users.noreply.github.com>
2024-06-29 01:52:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -47,4 +47,12 @@ document.addEventListener('DOMContentLoaded', () => {
             modal.style.display = "none";
         }
     }
+
+// image zoom
+    function zoomImage(imageUrl) {
+    // Open the image in a new window
+    var newWindow = window.open("", "_blank");
+    newWindow.document.write('<html><head><title>Lucifer</title></head><body style="margin:0;display:flex;justify-content:center;align-items:center;height:100vh;background-color:#000;"><img src="' + imageUrl + '" style="max-width:100%;max-height:100%;" alt="Zoomed Image"></body></html>');
+    newWindow.document.close();
+  }
 });

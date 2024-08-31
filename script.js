199b69c737d5b31ef134a546e6e60e1c2a1eeaaa
Ankit Raj <147795844+lucifer5094@users.noreply.github.com>
2024-08-31 06:18:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,90 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const faders = document.querySelectorAll('.fade-in');
+function zoomImage(imageUrl) {
+  // Open the image in a new window
+  var newWindow = window.open("", "_blank");
+  newWindow.document.write(
+    '<html><head><title>Lucifer</title></head><body style="margin:0;display:flex;justify-content:center;align-items:center;height:100vh;background-color:#000;"><img src="' +
+      imageUrl +
+      '" style="max-width:100%;max-height:100%;" alt="Zoomed Image"></body></html>'
+  );
+  newWindow.document.close();
 
-    const appearOptions = {
-        threshold: 0.5,
-        rootMargin: "0px 0px -50px 0px"
-    };
+  window.onload = function () {
+    alert("If you get any error \nkindly view in desktop mode");
+  };
+}
 
-    const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
-        entries.forEach(entry => {
-            if (!entry.isIntersecting) {
-                return;
-            } else {
-                entry.target.classList.add('appear');
-                appearOnScroll.unobserve(entry.target);
-            }
-        });
-    }, appearOptions);
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("commentForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
 
-    faders.forEach(fader => {
-        appearOnScroll.observe(fader);
+      const name = document.getElementById("name").value;
+      const email = document.getElementById("email").value;
+      const message = document.getElementById("message").value;
+
+      const mailtoLink = `mailto:ankitraj85455@gmail.com?subject=Comment from ${name}&body=Name: ${name}%0AEmail: ${email}%0AMessage: ${message}`;
+
+      window.location.href = mailtoLink;
+
+      alert("Your message has been sent!");
+      document.getElementById("commentForm").reset();
     });
 });
 
+const toggleButton = document.getElementById("mode-toggle");
 
-document.addEventListener('DOMContentLoaded', () => {
-    const modal = document.getElementById("imageModal");
-    const modalImg = document.getElementById("modalImage");
-    const captionText = document.getElementById("caption");
-    const previewBtns = document.querySelectorAll('.preview-btn');
-    const closeBtn = document.getElementsByClassName("close")[0];
+function setTheme(theme) {
+  document.documentElement.setAttribute("data-theme", theme);
+  localStorage.setItem("theme", theme);
+  toggleButton.textContent =
+    theme === "dark" ? "Switch to Light Mode" : "Switch to Dark Mode";
+}
 
-    previewBtns.forEach(btn => {
-        btn.addEventListener('click', function() {
-            modal.style.display = "block";
-            modalImg.src = this.getAttribute('data-full');
-            captionText.innerHTML = this.previousElementSibling.innerHTML;
-        });
-    });
+// Check for saved user preference, if any, on load
+const savedTheme = localStorage.getItem("theme") || "light";
+setTheme(savedTheme);
 
-    closeBtn.onclick = function() { 
-        modal.style.display = "none";
-    }
+toggleButton.addEventListener("click", () => {
+  const currentTheme = document.documentElement.getAttribute("data-theme");
+  const newTheme = currentTheme === "dark" ? "light" : "dark";
+  setTheme(newTheme);
+});
 
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
+// scroll
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const scrollElements = document.querySelectorAll('.scroll-animation');
+
+    const elementInView = (el, dividend = 1) => {
+      const elementTop = el.getBoundingClientRect().top;
+
+      return (
+        elementTop <=
+        (window.innerHeight || document.documentElement.clientHeight) / dividend
+      );
+    };
+
+    const displayScrollElement = (element) => {
+      element.classList.add('visible');
+    };
+
+    const hideScrollElement = (element) => {
+      element.classList.remove('visible');
+    };
+
+    const handleScrollAnimation = () => {
+      scrollElements.forEach((el) => {
+        if (elementInView(el, 1.25)) {
+          displayScrollElement(el);
+        } else {
+          hideScrollElement(el);
         }
-    }
+      });
+    };
 
-// image zoom
-   function zoomImage(imageUrl) {
-    // Open the image in a new window
-    var newWindow = window.open("", "_blank");
-    newWindow.document.write('<html><head><title>Lucifer</title></head><body style="margin:0;display:flex;justify-content:center;align-items:center;height:100vh;background-color:#000;"><img src="' + imageUrl + '" style="max-width:100%;max-height:100%;" alt="Zoomed Image"></body></html>');
-    newWindow.document.close();
-  }
+    window.addEventListener('scroll', () => {
+      handleScrollAnimation();
+    });
+  });
+
